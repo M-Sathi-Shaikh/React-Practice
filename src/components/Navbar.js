fe5154575a820ec60c8493ce21fd98bbef8bf6e8
M-Sathi-Shaikh <shaikhsathi41@gmail.com>
2024-12-03 10:18:27 +0500

    changed title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ function Navbar() {
   return (
     <nav className="bg-green-400 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-white">Hairnic</h1>
+        <h1 className="text-2xl font-bold text-white">Rajjib.Co</h1>
         <ul className="flex space-x-4">
           <li><a href="#" className="text-white hover:underline">Home</a></li>
           <li><a href="#" className="text-white hover:underline">About</a></li>
